bcd12e656aa6731f366ee979fdb343a84b3373aa
Ne-el <neel.mittal101@gmail.com>
2022-11-06 10:38:10 -0600

    i love codeine im going to fucking kill myself but it is okay, because i love cyberpatriot

---
+++ Bash/scripts/35_linpeas.sh
@@ -0,0 +1,26 @@
+function f_linpeas {
+    ((SCRIPT_NUM++))
+
+    if !  dpkg -l | grep git; then
+        read -p -r "Git isn't installed. Install? Y/n >" gitInstallYesNo
+
+        if [ "$gitInstallYesNo" == Y ]; then 
+            apt-get install git
+        else   
+            echo "Script: $SCRIPT_NUM ::: Git not installed and installation denied by user"
+            return
+        fi
+    fi
+
+    read -p -r "Would you like to run LinPEAS? Y/n >" linPeasYesNo
+
+    if [ "$linPeasYesNo" == Y ]; then  
+        curl -L https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas.sh
+        bash ./linpeas.sh
+    else
+        echo "Script: $SCRIPT_NUM ::: Git not installed and installation denied by user"
+        return
+    fi
+        
+
+}
